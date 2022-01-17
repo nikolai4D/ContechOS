deef6652cd5aa9dc06d7fe5c6af165940588dbdf
Maja Frankiewicz <majafrank@live.se>
2022-01-17 23:57:57 +0100

    DB3 Generic contextmenu WIP

--- public/components/ContextMenu.js
+++ public/components/ContextMenu.js
@@ -1,10 +1,22 @@
-const ContextMenu = (d) => {
+import nodeDefs from "../store/definitions.js";
 
-    const data = [{ "title": "Create", "id": "1" }]
+const ContextMenu = (d) => {
+    const nodeGroup = window.location.pathname.substring(1);
+
+    const validNodeTypesByNodeGroup = nodeDefs.nodeGroups.find(obj => {
+        return obj.title === nodeGroup;
+    }).nodeTypes;
+
+    const nodeTypesDetail = nodeDefs.nodeTypes.filter((type, index) => {
+        if (validNodeTypesByNodeGroup.includes(type.nodeTypeId))
+            return type
+    });
+
+    const data = nodeTypesDetail.map((type) => { return { "title": `Create ${type.title}`, "id": `${type.id}` } })
 
     let dataArray = data.map(obj =>
         `<li class="item">
-            <div class="itemContent" id="${obj.id}">
+            <div class="itemContent" id="${obj.nodeTypeId}">
                 ${obj.title}
             </div>
         </li>`);
@@ -20,6 +32,5 @@ const ContextMenu = (d) => {
     return template;
 }
 
-
-
 export default ContextMenu;
+
