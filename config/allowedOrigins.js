befccce393ed53bba3016ac29ada4e456e09f9e1
Maja <majafrank@live.se>
2022-01-31 23:49:18 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const allowedOrigins = [
     `${process.env.DEV_DOMAIN}:${process.env.PORT}`,
-    `${process.env.PROD_DOMAIN}:${process.env.PROD_PORT}`
+    `${process.env.PROD_DOMAIN}`
 ];
 
 module.exports = allowedOrigins;
