7513584bfa68b0be04622905c3ede4b2eb32ae34
Nicholas Alvén <nicholas@nikolaidev.com>
2022-02-02 10:37:42 +0100

    updated func for updatePwd and moved db dir outside of git

--- public/views/Datas.js
+++ public/views/Datas.js
@@ -1,20 +1,14 @@
-import Graph from "../components/Graph.js"
-import store from "../store/store.js"
-
+import Graph from "../components/Graph.js";
+import store from "../store/store.js";
 
 export default class Datas {
-    
-    constructor() {
-        document.title = "Data";
-    }
+  constructor() {
+    document.title = "Data";
+  }
 
-    async getTemplate() {
-        const view = "datas"
-        await store.GETALL(view)
-        return Graph(view)
-        
-        
-        
-    }
+  async getTemplate() {
+    const view = "datas";
+    await store.GETALL(view);
+    return Graph(view);
+  }
 }
-
