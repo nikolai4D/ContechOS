79534151a43f6164d81f8bb3d22ea23b456b262f
Maja Frankiewicz <majafrank@live.se>
2022-02-08 00:04:56 +0100

    Feature: Navbar links not disappearing when rescaling window

--- public/components/Navbar.js
+++ public/components/Navbar.js
@@ -1,31 +1,31 @@
 export default class Navbar {
-  cconstructor() {}
+  cconstructor() { }
 
   async getTemplate() {
     return `
-        <div id="navbar">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#" data-link="/">ContechOS</a>
-          <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-link="/props">Props</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-link="/configs">Config</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-link="/datas">Data</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <button class="btn btn-outline-secondary" type="button" data-link="/logout">Logout</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-        </div>
+    <div id="navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#" data-link="/">ContechOS</a>
+      <div class="navbar">
+        <ul class="navbar-nav me-auto" style="flex-direction:row;   justify-content: space-between;column-gap: 25px;">
+          <li class="nav-item">
+            <a class="nav-link" href="#" data-link="/props">Props</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" data-link="/configs">Config</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" data-link="/datas">Data</a>
+          </li>
+        </ul>
+        <form class="d-flex justify-content-end" style="margin-left:1.5em;">
+          <button class="btn btn-outline-secondary" type="button" data-link="/logout">Logout</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+    </div>
         `;
   }
 }
