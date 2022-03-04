5cf085690b6f4da30019f7bab2156df37949334b
Maja Frankiewicz <majafrank@live.se>
2022-03-04 19:13:29 +0100

    Feature: Sorted values in drop down list alphabetically

--- public/components/DropDownField.js
+++ public/components/DropDownField.js
@@ -1,7 +1,7 @@
 export default function dropDown(title, key, nodes, attr = null, labelValue = key, extra = "") {
 
     let firstOption = (attr !== 'multiple') ? `<option selected="true" disabled="disabled"></option>` : '';
-
+    nodes.sort((a, b) => a.title.localeCompare(b.title)))
     let options = (nodes.map((node, index) => {
         return `<option name="field_${key}_${index}" id="field_${key}_${index}" value="${node.id}">${node.title}</option>`
     })).join("");
