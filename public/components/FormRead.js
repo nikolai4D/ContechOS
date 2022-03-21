ba5fe28c3c8b699315db7988979751015db574cd
Maja Frankiewicz <majafrank@live.se>
2022-03-21 20:41:42 +0100

    Feature: Tooltip on parentId

--- public/components/FormRead.js
+++ public/components/FormRead.js
@@ -12,25 +12,42 @@ export function FormRead() {
     let defType = def.defTypes.find((obj) => obj.defTypeTitle === defTypeTitle);
     defType.defId = defId;
 
-    // let listWithAttributes = defType.attributes.map(attribute => {
-    //     return { [Object.keys(attribute)[0]]: State.clickedObj[Object.keys(attribute)[0]] }
-    // })
-
     let listWithAttributes = defType.attributes.map(attribute => {
-        console.log(State.clickedObj[Object.keys(attribute)[0]], 'hello')
+        let anInput = '';
+        console.log(Object.keys(attribute)[0], 'hello')
         if (State.clickedObj[Object.keys(attribute)[0]].length === 0) {
             return `    <div class="form-text">+ ${[Object.keys(attribute)[0]]}</div>
             `
         }
-        return `
-    <div class="">
-    <label for="staticEmail"  class="form-text">${[Object.keys(attribute)[0]]}</label>
-    <div>
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="${State.clickedObj[Object.keys(attribute)[0]]}">
-    </div>
+        else if (Object.keys(attribute)[0] === 'parentId') {
+            let parentId = State.clickedObj[Object.keys(attribute)[0]];
 
-    
-  </div>`})
+            let parent;
+            if (parentId.substring(0, 1) === 'c') {
+                let configs = JSON.parse(sessionStorage.getItem('configs'))[0].nodes;
+                parent = configs.find(config => config.id === parentId)
+            }
+            else {
+                let datas = JSON.parse(sessionStorage.getItem('datas'))[0].nodes;
+                parent = datas.find(data => data.id === parentId)
+
+            }
+            anInput = `<span class="d-inline-block" tabindex="0" data-bs-toggle="tooltip" title="${parent.title}">
+            <input type="text" readonly class="form-control-plaintext  p-1 bg-light rounded" id="staticEmail" value="${State.clickedObj[Object.keys(attribute)[0]]}">
+        </span>`
+        }
+        else {
+            anInput = `
+            <input type="text" readonly class="form-control-plaintext  p-1 bg-light rounded" id="staticEmail" value="${State.clickedObj[Object.keys(attribute)[0]]}">
+        `
+        }
+        return `
+            <div>
+                <label for="staticEmail"  class="form-text">${[Object.keys(attribute)[0]]}</label>
+            <div>
+            ${anInput}
+            </div>
+        </div>`})
 
     console.log(listWithAttributes)
 
