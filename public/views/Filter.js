db3db4a3b61eebdb89be7de68b6bb3a3098698b9
mattias <mattias@nikdev.io>
2022-10-12 17:28:58 +0200

    fixed placement of divs

--- public/views/Filter.js
+++ public/views/Filter.js
@@ -25,9 +25,9 @@ export default class Filter {
 
         sessionStorage.setItem("filter", JSON.stringify([{nodes: tree.selectedNodesData , rels: tree.visibleRelations }]));
         let rowDiv = createHtmlElementWithData("div", {"class": "row"})
-        let firstColumnDiv = createHtmlElementWithData("div", {"class": "col-sm", "id": "filterbox-grid-container-id"})
+        let firstColumnDiv = createHtmlElementWithData("div", {"class": "float-right", "id": "filterbox-grid-container-id"})
         rowDiv.appendChild(firstColumnDiv)
-        let secondColumnDiv = createHtmlElementWithData("div", {"class": "col-lg", "id": "data-display-grid-container-id"})
+        let secondColumnDiv = createHtmlElementWithData("div", {"class": "", "id": "data-display-grid-container-id"})
         rowDiv.appendChild(secondColumnDiv)
 
         const dataNodeAndRedrawFunc = await this.returnRenderFunc("filter")
@@ -38,13 +38,15 @@ export default class Filter {
         return [rowDiv]
     }
 
-    async displayDataAndFilter(firstColumnDiv, secondColumnDiv) {
+    async displayDataAndFilter(firstColumnDiv, secondColumnDiv, firstClass, secondClass) {
         const dataNodeAndRedrawFunc = await this.returnRenderFunc("filter")
         let filterBox = await FilterBox()
         setFilterBoxCallback(secondColumnDiv, filterBox, dataNodeAndRedrawFunc[1])
         firstColumnDiv.innerHTML = ""
+        firstColumnDiv.className = firstClass
         firstColumnDiv.appendChild(dataNodeAndRedrawFunc[0])
         secondColumnDiv.innerHTML = ""
+        secondColumnDiv.className = secondClass
         secondColumnDiv.appendChild(filterBox)
     }
 
@@ -53,11 +55,15 @@ export default class Filter {
             this.returnRenderFunc = renderDataAsGraph;
             this.displayDataAndFilter(
                 document.querySelector("#filterbox-grid-container-id"),
-                document.querySelector("#data-display-grid-container-id"));},
+                document.querySelector("#data-display-grid-container-id"),
+                "float-right",
+                "");},
         () =>  {
             this.returnRenderFunc = renderDataAsTable;
             this.displayDataAndFilter(
                 document.querySelector("#filterbox-grid-container-id"),
-                document.querySelector("#data-display-grid-container-id"));})}
+                document.querySelector("#data-display-grid-container-id"),
+                "col order-2",
+                "col order-1");})}
 
 }
