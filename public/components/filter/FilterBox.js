efd085e03a068859dfc969db0f452d30596d6bec
MattiasOlssonNikDev <90605095+MattiasOlssonNikDev@users.noreply.github.com>
2022-12-01 17:31:29 +0100

    C2 166 filter box toggle on each level (#123)
    * added hide button for sublists
    
    * changed button to icon
    
    * fix
    
    * fixed not breaking after each checkbox row
    
    * added chevron up icon

--- public/components/filter/FilterBox.js
+++ public/components/filter/FilterBox.js
@@ -1,5 +1,6 @@
 import {State} from "../../store/State.js";
 import { iconEye } from "./toggleHideShow.js";
+import { chevronDown, chevronUp } from ".././Icons.js";
 
 async function FilterBox() {
   let nodeHtmlString = await triggerTreeGetHtml();
@@ -47,15 +48,14 @@ async function triggerTreeGetHtml() {
 function itemRow(node){
     if(node.excluded) return ""
 
-    let childrenFrame = `<br/>`
+    let childrenFrame = ``
 
     if (node.selected === true && node.hasOwnProperty("children") && node.children.length !== 0){
       childrenFrame = `
-            <ul>
-            <input class="form-check-input" type="checkbox" value="" id="all_${node.id}"  ${ node.viewAll? "checked": ""}>
-            <label class="form-check-label text-break" for="all_${node.id}"> All</label>
-            <br/>
-          `
+            <ul style="margin-bottom:0;" id="acc_${node.id}-collapse" class="accordion-collapse collapse show" aria-labelledby="acc_${node.id}-heading">
+              <input class="form-check-input" type="checkbox" value="" id="all_${node.id}"  ${ node.viewAll? "checked": ""}>
+              <label class="form-check-label text-break" for="all_${node.id}"> All</label>
+              <br/>`
       
       node.children.sort((a, b) => a.title.localeCompare(b.title));
 
@@ -71,19 +71,37 @@ function itemRow(node){
     }
 
     const eye = node.hidden? iconEye(node.id).hide : iconEye(node.id).show
-
     const eyeDiv = `<div class="d-inline-block" id="toggleEyeContainer_${node.id}">${eye}</div>`
 
-     let mainRow = `
-     <div id="form-check-input-container" class="d-inline-block" role="button">
+    let mainrow = ""
+    if(childrenFrame !== ''){
+      mainrow = `
+        <div id="acc_${node.id}-heading" class="d-inline-block" role="button">
           <input class="form-check-input" type="checkbox" value="" id="checkbox_${node.id}" ${ node.selected? "checked": ""}>
           <label class="form-check-label text-break" for="checkbox_${node.id}"> ${node.title}</label> 
           ${ node.selected? eyeDiv : ""}
-          </div>
+          <button type="button" class="btn collapse show" id="acc_${node.id}-chevron-up" data-bs-toggle="collapse" style="transition-duration: 0s;"
+            data-bs-target="#acc_${node.id}-collapse,#acc_${node.id}-chevron-up,#acc_${node.id}-chevron-down,#acc_${node.id}-br" aria-expanded="false" aria-controls="acc_${node.id}-collapse">
+            ${chevronUp('role="button"')}
+          </button>
+          <button type="button" class="btn collapse" id="acc_${node.id}-chevron-down" data-bs-toggle="collapse" style="transition-duration: 0s;"
+            data-bs-target="#acc_${node.id}-collapse,#acc_${node.id}-chevron-up,#acc_${node.id}-chevron-down,#acc_${node.id}-br" aria-expanded="false" aria-controls="acc_${node.id}-collapse">
+            ${chevronDown('role="button"')}
+          </button>
+        </div> 
           ${ childrenFrame }
-     `
+        <br style="transition-duration: 0s;" id="acc_${node.id}-br" class="collapse"/>`
+    } else  {    
+      mainrow = `
+        <div id="form-check-input-container" class="d-inline-block" role="button">
+          <input class="form-check-input" type="checkbox" value="" id="checkbox_${node.id}" ${ node.selected? "checked": ""}>
+          <label class="form-check-label text-break" for="checkbox_${node.id}"> ${node.title}</label> 
+          ${ node.selected? eyeDiv : ""}
+        </div>
+        <br/>`
+    }
 
-    return mainRow
+    return mainrow
 }
 
 
